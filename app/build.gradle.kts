6571bb28d5e5beff0ec1337e18c73832b783e526
Alexis <alexis.borges@estudiantes.utec.edu.uy>
2024-10-30 23:38:47 -0300

    quitado cosas incecesarias, aniadido dependencias para google auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,16 @@ dependencies {
     // Firebase auth
     implementation("com.google.firebase:firebase-auth")
 
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth-ktx")
+
+    // Also add the dependency for the Google Play services library and specify its version
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
